723da616fb28b0bc440b94aab1a209c5fc500d9b
HuangShuao <1147690359@qq.com>
2022-04-13 17:21:37 +0800

    登录

---
+++ mybatis/src/main/java/com/example/mybatis/controller/Mapping.java
@@ -0,0 +1,17 @@
+package com.example.mybatis.controller;
+
+
+import com.example.mybatis.userbean.user.Userbean;
+import org.springframework.web.bind.annotation.*;
+
+public class Mapping {
+
+
+
+
+
+
+
+
+
+}
